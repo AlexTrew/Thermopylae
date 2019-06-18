6f0b1a862ad2d5aa8dd573c43b73c9450acd3752
alex <alextrew.ba@gmail.com>
2019-06-18 18:59:30 +0100

    finished refactor

--- vector2.h
+++ vector2.h
@@ -2,6 +2,8 @@
 // Created by alex on 18/06/19.
 //
 
+#ifndef PRACTICE_VECTOR2_H
+#define PRACTICE_VECTOR2_H
 
 struct vector2 {
     int x;
@@ -9,7 +11,6 @@ struct vector2 {
 };
 
 
-#ifndef PRACTICE_VECTOR2_H
-#define PRACTICE_VECTOR2_H
+
 
 #endif //PRACTICE_VECTOR2_H
